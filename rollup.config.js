ff26c1c3d5d2106951dd4c51ba2799673b7bbd47
Andreas Rozek <A.Rozek@gmx.de>
2021-06-23 17:21:32 +0200

    update build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 // see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
 
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/svelte-drag-and-drop-actions.ts',
   output: {
     file:     './dist/svelte-drag-and-drop-actions.esm.js',
     format:   'esm',
-    sourcemap:true
+    sourcemap:true,
   },
-  plugins: [typescript(), terser({ format:{ comments:false, safari10:true } })],
+  plugins: [
+    resolve(), commonjs(), typescript(),
+  ],
 };
