d380d9f4cb1f652976f7d7d4e4d858b248f92879
Andreas Rozek <A.Rozek@gmx.de>
2021-07-05 13:04:13 +0200

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
-// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+// see https://github.com/rozek/build-configuration-study
 
 import commonjs   from '@rollup/plugin-commonjs'
 import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: './src/svelte-drag-and-drop-actions.ts',
+  input:   './src/svelte-drag-and-drop-actions.ts',
+  external:['javascript-interface-library','svelte-coordinate-conversion'],
   output: {
     file:     './dist/svelte-drag-and-drop-actions.esm.js',
     format:   'esm',
     sourcemap:true,
   },
   plugins: [
-    resolve(), commonjs(), typescript(),
+    resolve({ browser:true, dedupe:['svelte'] }), commonjs(), typescript(),
   ],
 };
